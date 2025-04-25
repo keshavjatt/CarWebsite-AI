298577ead51848b49009e6be0a5490ec9ce1af59
keshavjatt <thecoder780@gmail.com>
2025-04-25 17:53:30 +0530

    worked on deployment ready project

--- app/layout.js
+++ app/layout.js
@@ -7,7 +7,7 @@ import Header from "@/components/Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Vehiqle",
+  title: "Vehiqle AI",
   description: "Find your Dream Car",
 };
 
