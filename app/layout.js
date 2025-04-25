04d30bfa4f2dd6d254dc664f6eb9bc81ebade9e0
keshavjatt <thecoder780@gmail.com>
2025-04-25 15:45:47 +0530

    worked on deployment ready project

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
 
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
-              <p>Made with 💗 by RoadsideCoder</p>
+              <p>Made with 💗 by Keshav</p>
             </div>
           </footer>
         </body>
